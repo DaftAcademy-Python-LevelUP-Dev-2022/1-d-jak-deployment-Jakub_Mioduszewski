8ebf296b7c4c8bb4599c84959c80f831433b1729
Miodek <kedoim420@gmail.com>
2022-05-14 13:42:25 +0200

    2nd try

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 class HerokuApp:
     app_url = ""  # Fill your heroku app url here
 
-from fastapi import FastAPI
 
-app = FastAPI()
-@app.get("/")
-def root():
-    return {"start":"1970-01-01"}
 
